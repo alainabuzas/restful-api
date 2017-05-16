649c74314ea8d7a5c0edfde03a19f0abeb57b261
alainabuzas <alaina.buzas@gmail.com>
2017-05-16 11:26:51 -0700

    starting lab

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+var bodyPareser = require('body-parser');
+var mongoose = require('mongoose');
+var expressJWT = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var User = require('./models/user');
+var secret = "orcasecretsadmin";
 
 app.use(require('morgan')('dev'));
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+    res.send('Hello Backend!');
 });
 
 var server = app.listen(process.env.PORT || 3000);
